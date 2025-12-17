64f4186bab0a8c82134710aa797906b65ffcb646
zhangchuncheng <south.orange.zh@gmail.com>
2025-12-18 01:31:48 +0800

    学习

---
+++ external/static.djicdn.com/sentry-sdk/manifest-latest.js
@@ -0,0 +1,16 @@
+var __SENTRY_MANIFEST_DATA__ = {
+  "currentVersion": "0.0.8",
+  "versions": [
+    "0.0.1",
+    "0.0.2",
+    "0.0.3",
+    "0.0.4",
+    "0.0.5",
+    "0.0.6",
+    "0.0.7",
+    "0.0.8",
+  ],
+  "description": "动态规则0.0.8版本",
+  "authorAd": "tushang.yang"
+}
+
